252c39f96d9242a147539e2d998b47341b248d31
ffernn-dev <hihi.fernguy@gmail.com>
2023-05-09 21:11:00 +1000

    Small changes? (thats a lie)
    Former-commit-id: 80ff7b30de37a044570f292115f468039010c7b9

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let root = document.documentElement;
+let root = document.documentElement
 let rootStyle = getComputedStyle(root);
 
 const urlParams = new URLSearchParams(window.location.search);
@@ -13,55 +13,60 @@ function show(shown) {
 };
 
 function showpage(page){
-	if(page == 'urlparams' && urlParams.get('page')) {
-		page = urlParams.get('page')
-	}
 	if(page) {
 		show(page);
 		window.history.replaceState(null, null, "?page=" + page);
 	} else {
-		show("home");
+		if(urlParams.get('page')) {
+			show(urlParams.get('page'))
+		} else {
+			show("home");
+		}
 	}
 }
 
 function switchtheme() {
-	if (rootStyle.getPropertyValue("--theme") == "dark") {
-		root.style.setProperty("--theme", "light");
-		root.style.setProperty("--background-1", "#f7fff7");
-		root.style.setProperty("--background-2", "#fffaf3");
-		root.style.setProperty("--foreground", "#575279");
-		root.style.setProperty("--button", "#eb6f92");
-		root.style.setProperty("--secondary", "#eb6f92");
+	const currentTheme = rootStyle.getPropertyValue("--theme")
+	if (currentTheme == "dark") {
+		setProp("theme", "light")
+		document.cookie ="theme=light; expires=2147483647;"
+		setProp("background-1", "#f7fff7");
+		setProp("background-2", "#fffaf3");
+		setProp("foreground", "#575279");
+		setProp("button", "#eb6f92");
+		setProp("secondary", "#eb6f92");
 		for (var i of document.getElementsByClassName("svg")) {
 			i.setAttribute("fill", "#1c1a1e");
 		}
 	}
 
-	else if (rootStyle.getPropertyValue("--theme") == "light") {
-		root.style.setProperty("--theme", "dark");
-		root.style.setProperty("--background-1", "#191724");
-		root.style.setProperty("--background-2", "#1f1d2e");
-		root.style.setProperty("--foreground", "#e0def4");
-		root.style.setProperty("--button", "#31748f");
-		root.style.setProperty("--secondary", "#eb6f92");
+	else if (currentTheme == "light") {
+		setProp("theme", "dark");
+		document.cookie ="theme=dark; expires=2147483647;"
+		setProp("background-1", "#191724");
+		setProp("background-2", "#1f1d2e");
+		setProp("foreground", "#e0def4");
+		setProp("button", "#31748f");
+		setProp("secondary", "#eb6f92");
 		for (var i of document.getElementsByClassName("svg")) {
 			i.setAttribute("fill", "#e0def4")
 		}
 	}
+}
 
-	else {
-		root.style.setProperty("--theme", "dark");
-		root.style.setProperty("--background-1", "#1c1a1e");
-		root.style.setProperty("--background-2", "#221f23");
-		root.style.setProperty("--foreground", "#f7fff7");
-		root.style.setProperty("--button", "#38B863");
-		root.style.setProperty("--secondary", "#EF476F");
-		for (var i of document.getElementsByClassName("svg")) {
-			i.setAttribute("fill", "#f7fff7")
-		}
-	}
+function setProp(name, value) {
+	root.style.setProperty('--' + name, value);
 }
 
 window.onload = function() {
-	showpage('urlparams')
+	if (getCookie("theme") == "light") {
+		switchtheme();
+	}
+	showpage()
+}
+
+function getCookie(name) {
+	const value = `; ${document.cookie}`;
+	const parts = value.split(`; ${name}=`);
+	if (parts.length === 2) return parts.pop().split(';').shift();
 }
